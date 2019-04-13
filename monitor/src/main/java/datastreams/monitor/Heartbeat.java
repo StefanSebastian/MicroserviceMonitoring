c57deddb8376a1e920e7b38a8366ebadf51e8752
StefanSebastian <stefansebii@gmail.com>
2019-04-13 19:05:18 +0300

    Attempt to build monitor

---
+++ monitor/src/main/java/datastreams/monitor/Heartbeat.java
@@ -0,0 +1,29 @@
+package datastreams.monitor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author stefansebii@gmail.com
+ */
+@Component
+public class Heartbeat {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    public Heartbeat() {
+        log.info("Starting heartbeat service");
+    }
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 2000)
+    public void reportCurrentTime() {
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
---
+++ monitor/src/main/java/datastreams/monitor/Main.java
@@ -0,0 +1,14 @@
+package datastreams.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @author stefansebii@gmail.com
+ */
+@EnableAutoConfiguration
+public class Main {
+    public static void main(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
+}
