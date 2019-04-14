0a2602c2a5540a1e1671dcf88e1fa72370557e84
StefanSebastian <stefansebii@gmail.com>
2019-04-14 21:40:45 +0300

    Some configuration for kafka messages

--- monitorclient/src/main/java/com/example/client/TaskExecutor.java
+++ monitorclient/src/main/java/com/example/client/TaskExecutor.java
@@ -1,19 +1,50 @@
 package com.example.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.Date;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 
 @Configuration
 @ComponentScan(basePackages = "com.example.client")
 public class TaskExecutor {
-	
+
+    @Value("${kafka.topic.name}")
+    private String topicName;
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    private long pid;
+    private String machine;
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public TaskExecutor(){
+        RuntimeMXBean bean = ManagementFactory.getRuntimeMXBean();
+
+        String jvmName = bean.getName();
+        pid = Long.valueOf(jvmName.split("@")[0]);
+        machine = jvmName.split("@")[1];
+    }
 	
 	@Scheduled(fixedRate = 2000)
-    public void reportCurrentTime() {
-        System.out.println("My client Current date is --->"+ new Date());
+    public void heartbeat() {
+	    long timestamp = System.currentTimeMillis();
+        HeartbeatDTO heartbeatDTO = new HeartbeatDTO();
+        heartbeatDTO.setMachine(machine);
+        heartbeatDTO.setPid(pid);
+        heartbeatDTO.setMicroserviceName(appName);
+        heartbeatDTO.setTimestamp(timestamp);
+
+        System.out.println(heartbeatDTO);
+        kafkaTemplate.send(topicName, heartbeatDTO.toString());
     }
 
 }
