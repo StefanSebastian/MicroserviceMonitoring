d94898e735ebc02a2914b1eb2a470e1bac4d8923
Stefan Sebastian <sstefan@montran.com>
2020-03-22 12:57:38 +0200

    Started work on a scaling manager

---
+++ scaling-manager/src/main/java/microservice/scalingmanager/CommandsRepository.java
@@ -0,0 +1,65 @@
+package microservice.scalingmanager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class CommandsRepository {
+
+    Logger log = LoggerFactory.getLogger(CommandsRepository.class);
+
+    private static final String startupCmdsFile = "startup_commands.txt";
+    private static final String shutdownCmdsFile = "shutdown_commands.txt";
+    private static final String delimiter = ":";
+
+    private Map<String, String> startupCmds = new HashMap<>();
+    private Map<String, String> shutdownCmds = new HashMap<>();
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initCommands() {
+        File startupCmdFile = getFileFromResources(startupCmdsFile);
+        startupCmds = readFileIntoMap(startupCmdFile);
+
+        File shutdownCmdFile = getFileFromResources(shutdownCmdsFile);
+        shutdownCmds = readFileIntoMap(shutdownCmdFile);
+
+        log.info(startupCmds.toString());
+        log.info(shutdownCmds.toString());
+    }
+
+    private File getFileFromResources(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("File not found!");
+        }
+        return new File(resource.getFile());
+    }
+
+    private Map<String, String> readFileIntoMap(File file) {
+        Map<String, String> map = new HashMap<>();
+        try(FileReader reader = new FileReader(file)) {
+            BufferedReader br = new BufferedReader(reader);
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(delimiter, 2);
+                if (parts.length >= 2) {
+                    map.put(parts[0], parts[1]);
+                } else {
+                    log.info("Ignoring line : " + line);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Could not initialize control commands");
+        }
+        return map;
+    }
+}
---
+++ scaling-manager/src/main/java/microservice/scalingmanager/ScalingManagerApplication.java
@@ -0,0 +1,13 @@
+package microservice.scalingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalingManagerApplication.class, args);
+	}
+
+}
