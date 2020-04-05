08ea794c60b95426d303783235608ada07ade83a
Stefan Sebastian <sstefan@montran.com>
2020-04-05 15:58:18 +0300

    Plots and reports

--- scaling_research/prepare_data/explore.py
+++ scaling_research/prepare_data/explore.py
@@ -7,8 +7,12 @@ from prep_config import ts_file
 
 df = pd.read_csv(ts_file, header=None, names=["Ts"])
 df["Ts"] = pd.to_datetime(df["Ts"], unit='s')
-df['Count'] = 1
+df["Count"] = 1
+df.set_index("Ts", inplace=True)
 df.info()
 
-df.set_index('Ts', inplace=True)
-
+print("-------------------------")
+gdf = df.groupby(pd.Grouper(freq='5min')).sum()
+gdf.info()
+gdf.plot()
+plt.show()
