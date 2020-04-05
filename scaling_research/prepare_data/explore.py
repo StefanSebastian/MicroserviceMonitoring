23a8872851ca696522ee8c9e7a59afaa7c8dddc8
Stefan Sebastian <sstefan@montran.com>
2020-04-05 14:15:29 +0300

    Data analyssi

--- scaling_research/prepare_data/explore.py
+++ scaling_research/prepare_data/explore.py
@@ -1,6 +1,14 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns 
 
 from prep_config import ts_file
 
-print(ts_file)
+df = pd.read_csv(ts_file, header=None, names=["Ts"])
+df["Ts"] = pd.to_datetime(df["Ts"], unit='s')
+df['Count'] = 1
+df.info()
+
+df.set_index('Ts', inplace=True)
 
