87bc4f7878f6b21c16b811848748443701cecc4b
Stefan Sebastian <sstefan@montran.com>
2020-04-06 19:35:34 +0300

    Normalization

--- scaling_research/prepare_data/prep_config.py
+++ scaling_research/prepare_data/prep_config.py
@@ -1,2 +1,4 @@
-ts_file = '../datasets/wikipedia/raw_jp/requests.txt'
+ts_file = '../datasets/wikipedia/raw_ro/requests.txt'
+window_size = '5min'
+feature_no = 10
 
---
+++ scaling_research/prepare_data/transform.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+from prep_config import ts_file, window_size, feature_no
+
+df = pd.read_csv(ts_file, header=None, names=["Ts"])
+df["Ts"] = pd.to_datetime(df["Ts"], unit='s')
+df["Count"] = 1
+df.set_index("Ts", inplace=True)
+gdf = df.groupby(pd.Grouper(freq=window_size)).sum()
+
+data = np.asarray(gdf["Count"])
+np.savetxt('grouped.txt', data, delimiter=',', fmt='%d')
+
+min_d = np.amin(data)
+max_d = np.amax(data)
+normalizer = lambda t: (t - min_d) / (max_d - min_d)
+normalized = np.array([normalizer(xi) for xi in data])
+np.savetxt('normalized.txt', normalized, delimiter=',', fmt='%f')
+
