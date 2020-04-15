0a63259cb631defed5d646d105ac5dfb8ecadad8
Stefan Sebastian <sstefan@montran.com>
2020-04-15 17:51:10 +0300

    Deleted prev report; test model load

--- scaling_research/ml/test.py
+++ scaling_research/ml/test.py
@@ -9,6 +9,7 @@ import numpy as np
 from datetime import datetime
 import os
 from shutil import copyfile
+from keras.models import model_from_json
 
 from preparedata import get_data
 from config import epochs, batch_size, report_dir
@@ -65,6 +66,16 @@ def store_model(rep_folder, model):
     # serialize weights to HDF5
     model.save_weights(rep_folder + "/model.h5")
     print("Saved model to disk")
+
+def load_model(model_dir):
+    json_file = open(model_dir + '/model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights(model_dir + "/model.h5")
+    print("Loaded model from disk")
+    return loaded_model
 
 if (__name__ == '__main__'):
     # create report folder 
