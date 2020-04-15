30a9c8af9290504529f55ef3afe092fbb94a18b2
Stefan Sebastian <sstefan@montran.com>
2020-04-15 18:08:34 +0300

    Fixed mape fc; added a good report

--- scaling_research/ml/test.py
+++ scaling_research/ml/test.py
@@ -33,9 +33,17 @@ def plot_predictions(Y_test, y_predicted, rep_folder):
     plt.legend((test_ln, predicted_ln), ("Actual", "Predicted"))
     plt.savefig(rep_folder + "/predictions.png")
 
+def percentage_error(actual, predicted):
+    res = np.empty(actual.shape)
+    for j in range(actual.shape[0]):
+        if actual[j] != 0:
+            res[j] = (actual[j] - predicted[j]) / actual[j]
+        else:
+            res[j] = predicted[j] / np.mean(actual)
+    return res
+
 def mean_absolute_percentage_error(y_true, y_pred): 
-    y_true, y_pred = np.array(y_true), np.array(y_pred)
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return np.mean(np.abs(percentage_error(np.asarray(y_true), np.asarray(y_pred)))) * 100
 
 def report_performance(Y_test, y_predicted):
     mse = mean_squared_error(Y_test, y_predicted)
