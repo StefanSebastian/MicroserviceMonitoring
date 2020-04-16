1f90cdc0fb9b02ba8df18991526cac30f3e2468c
Stefan Sebastian <sstefan@montran.com>
2020-04-16 16:55:36 +0300

    More reports

---
+++ scaling_research/reports/1587045045/config.py
@@ -0,0 +1,12 @@
+data_file = 'datasets/wikipedia/raw_jp/5min/grouped.txt'
+train_test_split = 0.9
+
+feature_no = 24
+cnn_seq = 6
+cnn_steps_per_seq = 4
+lstm_units = 50
+
+epochs = 100
+batch_size = 5
+
+report_dir = "reports/"
