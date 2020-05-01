ae44f7094efc6c1911043490a1bfcf159e083354
Stefan Sebastian <stefansebii@gmail.com>
2020-05-01 11:59:26 +0300

    Added new report on cnn_lstms

---
+++ scaling_research/reports/test_runs/jp10/11m_cnn_jp10/config.py
@@ -0,0 +1,13 @@
+data_file = 'datasets/wikipedia/raw_jp/10min/grouped.txt'
+train_test_split = 0.9
+
+feature_no = 128
+cnn_seq = 14
+lstm_units = 150
+dense_units = 250
+
+epochs = 100
+batch_size = 8
+
+report_dir = "reports/"
+test_iterations = 10
