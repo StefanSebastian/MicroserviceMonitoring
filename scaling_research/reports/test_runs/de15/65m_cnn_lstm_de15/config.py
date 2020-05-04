deea836e2f30c8c08d4f8b8cc7579ed09591161f
Stefan Sebastian <sstefan@montran.com>
2020-05-04 22:26:54 +0300

    New de 15 report

---
+++ scaling_research/reports/test_runs/de15/65m_cnn_lstm_de15/config.py
@@ -0,0 +1,13 @@
+data_file = 'datasets/wikipedia/raw_de/15min/grouped.txt'
+train_test_split = 0.9
+
+feature_no = 168
+cnn_seq = 14
+lstm_units = 150
+dense_units = 250
+
+epochs = 200
+batch_size = 8
+
+report_dir = "reports/"
+test_iterations = 5
