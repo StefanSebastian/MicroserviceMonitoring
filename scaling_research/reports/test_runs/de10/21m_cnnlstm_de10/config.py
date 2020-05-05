e3430c09bb2f09df44f4c2e8e69a1c305e933f58
Stefan Sebastian <stefansebii@gmail.com>
2020-05-05 13:57:24 +0300

    New report

---
+++ scaling_research/reports/test_runs/de10/21m_cnnlstm_de10/config.py
@@ -0,0 +1,13 @@
+data_file = 'datasets/wikipedia_extracted/de/10min/grouped.txt'
+train_test_split = 0.9
+
+feature_no = 256
+cnn_seq = 16
+lstm_units = 150
+dense_units = 250
+
+epochs = 200
+batch_size = 8
+
+report_dir = "reports/"
+test_iterations = 10
