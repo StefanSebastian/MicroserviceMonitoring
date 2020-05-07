0735ad093158170c5622ed865d11bd8e99dff2ad
Stefan Sebastian <sstefan@montran.com>
2020-05-07 19:01:39 +0300

    Nice grid display

--- monitor_scaler_app/monitor-dashboard/src/Dashboard.js
+++ monitor_scaler_app/monitor-dashboard/src/Dashboard.js
@@ -5,16 +5,25 @@ import AverageRequestTimes from './AverageRequestTimes';
 import SLAStats from './SLAStats';
 
 class Dashboard extends Component {
+
     render() {
         return(
-            <div>
-                <OnlineServices />
-                <SLAStats />
-                <RequestsPerService />
-                <AverageRequestTimes />
-            </div>
+            <section style={sectionStyle}>
+                <div><OnlineServices /></div>
+                <div><SLAStats /></div>
+                <div><RequestsPerService /></div>
+                <div><AverageRequestTimes /></div>    
+            </section>
         )
     }
+}
+
+const sectionStyle = {
+    display: "grid",
+    gridTemplateColumns: '1fr 1fr',
+    gridTemplateRows: 'auto auto',
+    gridGap: "10px",
+    gridAutoFlow: "column"
 }
 
 export default Dashboard;
