fb303de1613c69ddda675f142f52c63b731000b5
Stefan Sebastian <sstefan@montran.com>
2020-05-10 13:05:33 +0300

    more work on laod test

--- demoapp/message_sender/src/main/java/sender/load/LoadMain.java
+++ demoapp/message_sender/src/main/java/sender/load/LoadMain.java
@@ -3,8 +3,7 @@ package sender.load;
 public class LoadMain {
 	
 	public static void main(String[] args) {
-		Integer users = 100;//Integer.valueOf(args[0]);
 		LoadTest tester = new LoadTest();
-		tester.startUsers(users);
+		tester.performTest();
 	}
 }
